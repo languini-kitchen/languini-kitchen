a7402c62c350abefe8631b0a018030b10dd8dd15
Anton Schäfer <scanton@ethz.ch>
2024-04-23 16:37:08 +0200

    Fix debug script

--- debug.sh
+++ debug.sh
@@ -8,5 +8,5 @@ project=gpt
 export WANDB_MODE=disabled
 
 # use "yes" to automatically confirm overwrties of old project folders
-./venv/bin/torchrun --standalone -m pdb languini/projects/$project/main.py tiny --train_batch_size 8 --gradient_accumulation_steps 4 --max_train_steps 512 --decay_steps 512 --compile None
+./venv/bin/torchrun -m pdb languini/projects/$project/main.py tiny --train_batch_size 8 --gradient_accumulation_steps 4 --max_train_steps 512 --decay_steps 512 --compile None --logger_type tb
 
